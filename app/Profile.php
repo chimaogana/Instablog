080237f9b08549c26599a6a9341014e8a018018d
chimaogana <clevadani@gmail.com>
2020-09-26 10:39:55 +0100

    First commit

---
+++ app/Profile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : 'profile/o8THXOoHlzwYYXmytDqpLP8VgF9BaUb61mZBnu43.jpeg';
+        return '/storage/' .$imagePath;
+    }
+    public function followers(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
